de6f312ab93ae04ae40a02dac1c29662d7d62e6d
EstherOkiri <est.okiri@gmail.com>
2025-10-26 23:04:00 +0300

    created database models

--- backend-django/lms_api/main/models.py
+++ backend-django/lms_api/main/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+#instructor model
+class Instructor(models.Model):
+    name= models.CharField(max_length=100)
+    email= models.CharField(max_length=100)
+    password= models.CharField(max_length=100)
+    qualification=models.CharField(max_length=200)
+    mobile=models.CharField(max_length=20)
+
+#course category model
+class CourseCategory(models.Model):
+    category_title=models.CharField(max_length=200)
+    category_description=models.TextField()
+
+    class Meta:
+        verbose_name_plural= "Course Categories"
+
+#Course Model
+
+class Course(models.Model):
+    category=models.ForeignKey(CourseCategory, on_delete=models.CASCADE)
+    instructor=models.ForeignKey(Instructor, on_delete=models.CASCADE)
+    title=models.CharField(max_length=200)
+    description=models.TextField()
+
+#Student Model
+class Student(models.Model):
+    name=models.CharField(max_length=100)
+    email=models.CharField(max_length=100)
+    password=models.CharField(max_length=100)
+    mobile=models.CharField(max_length=20)
+    address=models.TextField()
