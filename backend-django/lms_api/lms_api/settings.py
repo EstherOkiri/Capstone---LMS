19996b91a01e1787c2d0bcbc3b04f62a0e3eb662
EstherOkiri <est.okiri@gmail.com>
2025-10-26 21:47:23 +0300

    add postgres

--- backend-django/lms_api/lms_api/settings.py
+++ backend-django/lms_api/lms_api/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'lms_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'lms_database',
+        'USER' : 'lms_admin',
+        'PASSWORD' : 'admin',
+        'HOST' : 'localhost',
+        'PORT' : '5432',
     }
 }
 
