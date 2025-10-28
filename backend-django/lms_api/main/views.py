ec5680e446840f3712663119af6599da5dc47d41
EstherOkiri <est.okiri@gmail.com>
2025-10-28 19:57:19 +0300

    added Instructor api view

--- backend-django/lms_api/main/views.py
+++ backend-django/lms_api/main/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializer import InstructorSerializer
+from . import models
 
 # Create your views here.
+
+class InstructorList(APIView):
+    def get(self,request):
+        instructor=models.Instructor.objects.all()
+        serializer=InstructorSerializer(instructor, many=True)
+        return Response(serializer.data)
