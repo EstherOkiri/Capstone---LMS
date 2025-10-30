16781bc8dcbefc78786b02732e85946f74b631db
EstherOkiri <est.okiri@gmail.com>
2025-10-30 19:42:54 +0300

    DRF Authentication added

--- backend-django/lms_api/main/views.py
+++ backend-django/lms_api/main/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import generics
+from rest_framework import permissions
 from .serializer import InstructorSerializer
 from . import models
 
 # Create your views here.
 
-class InstructorList(APIView):
-    def get(self,request):
-        instructor=models.Instructor.objects.all()
-        serializer=InstructorSerializer(instructor, many=True)
-        return Response(serializer.data)
+class InstructorList(generics.ListCreateAPIView):
+    queryset=models.Instructor.objects.all()
+    serializer_class=InstructorSerializer
+    permission_classes=[permissions.IsAuthenticated]
+
+class InstructorDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset=models.Instructor.objects.all()
+    serializer_class=InstructorSerializer
+
+
