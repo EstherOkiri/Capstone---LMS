8ceedf1ff1ae3660fc96b0a9cb1e686c99bd3fed
EstherOkiri <est.okiri@gmail.com>
2025-11-03 20:09:50 +0300

    Put cards for the courses

--- lms_frontend/src/components/Home.js
+++ lms_frontend/src/components/Home.js
@@ -2,8 +2,50 @@ import Navbar from "./Navbar";
 
 function Home() {
   return (
+    
     <div className="container mt-4">
+      /*Latest Courses*/
       <h3 className="border-bottom pb-1 mb-4">Courses</h3>
+      <div className="row">
+        <div className="col-md-3">
+          <div className="card" >
+            <a href= "#"><img src="logo512.png" className="card-img-top" alt="..."/></a>
+            <div className="card-body">
+              <h5 className="card-title">Course Title</h5>
+              <a href="#" className="btn btn-primary">See More</a>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-3">
+          <div className="card" >
+            <a href= "#"><img src="logo512.png" className="card-img-top" alt="..."/></a>
+            <div className="card-body">
+              <h5 className="card-title">Course Title</h5>
+              <a href="#" className="btn btn-primary">See More</a>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-3">
+          <div className="card" >
+            <a href= "#"><img src="logo512.png" className="card-img-top" alt="..."/></a>
+            <div className="card-body">
+              <h5 className="card-title">Course Title</h5>
+              <a href="#" className="btn btn-primary">See More</a>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-3">
+          <div className="card" >
+            <a href= "#"><img src="logo512.png" className="card-img-top" alt="..."/></a>
+            <div className="card-body">
+              <h5 className="card-title">Course Title</h5>
+              <a href="#" className="btn btn-primary">See More</a>
+            </div>
+          </div>
+        </div>
+      </div>
+{/*Popular Courses*/}
+      <h3 className="border-bottom pb-1 mb-4 mt-4">Popular Courses</h3>
       <div className="row">
         <div className="col-md-3">
           <div className="card" >
@@ -44,7 +86,7 @@ function Home() {
       </div>
       
     </div>
-   
+    
     
   );
 }
