ae64390d2ebde9b843d6ae183fd7ac1fc7789153
EstherOkiri <est.okiri@gmail.com>
2025-11-03 21:32:11 +0300

    add footer

---
+++ lms_frontend/src/components/Footer.js
@@ -0,0 +1,18 @@
+function Footer(){
+    return(
+        <footer>
+        <div className="text-muted py-5 border-top mt-5">
+            <div className="container">
+                <p className="float-end mb-1">
+                    <a href='#'>Back to top</a>
+                </p>
+                <p className="mb-1">XXXXXXXXXXXXX</p>
+                <p className="mb-0">XXXX <a href= "/">Homepage</a></p>
+            </div>
+        </div>
+    </footer>
+    )
+    
+}
+
+export default Footer
